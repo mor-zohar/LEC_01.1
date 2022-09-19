ee9afb2dd6561ffafcb5763658916ccbe6fb6427
Mor <Mor@DESKTOP-MCJJLRB>
2022-09-19 18:30:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Lesson1_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is line
+            /*this is comment*/
+           
+            
+ /*          //פלט
+            Console.WriteLine("hello world");
+
+            //קלט
+            Console.ReadLine();
+*/
+            // VARIABLE
+            string firstName = "";
+            string name = "mor";
+            int age = 28;
+            Console.WriteLine("hello "+name+" your age is: "+age);
+            Console.WriteLine("hello {0} your age is: {1}",name,age);
+        }
+    }
+}
